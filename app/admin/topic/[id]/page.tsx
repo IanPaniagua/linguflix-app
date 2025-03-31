062d39b8248010d1c52b234d533be9aa91d7ffae
IanPaniagua <paniagua.ian.de@gmail.com>
2025-03-31 19:22:58 +0200

    trying to solve params string id error

--- app/admin/topic/[id]/page.tsx
+++ app/admin/topic/[id]/page.tsx
@@ -1,4 +1,3 @@
-import { use } from 'react'
 import TopicForm from './TopicForm'
 
 // Indicamos explícitamente que esto es una página de servidor
@@ -6,11 +5,8 @@ export const dynamic = 'force-dynamic'
 export const fetchCache = 'default-no-store'
 
 // Seguimos la forma exacta de la documentación oficial
-export default function Page(props: {
-  params: Promise<{ id: string }>
-}) {
-  // Usamos el hook 'use' para componentes síncronos
-  const params = use(props.params)
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params
   const id = params.id
   
   return <TopicForm id={id} />
