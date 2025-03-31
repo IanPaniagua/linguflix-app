06275d88cf9161f50e99f8f90e80dce6b76870d1
IanPaniagua <paniagua.ian.de@gmail.com>
2025-03-31 19:03:38 +0200

    using promise for params

--- app/admin/topic/[id]/page.tsx
+++ app/admin/topic/[id]/page.tsx
@@ -1,7 +1,16 @@
 import TopicForm from './TopicForm'
 
-export default async function Page({ params }: { params: any }) {
-  const resolvedParams = params instanceof Promise ? await params : params
+// Definimos que params es una promesa según la documentación oficial
+type Params = Promise<{ id: string }>
+
+// Usamos un componente async para poder usar await
+export default async function Page({
+  params,
+}: {
+  params: Params
+}) {
+  // Esperamos a que se resuelva la promesa de params
+  const resolvedParams = await params
   const id = resolvedParams.id
   
   return <TopicForm id={id} />
