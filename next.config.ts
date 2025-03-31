a910920dc00f3bbaffe12a9759c66da73837c23b
IanPaniagua <paniagua.ian.de@gmail.com>
2025-03-31 19:42:22 +0200

    using .ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        pathname: '/**',
+      }
+    ],
+    formats: ['image/webp'],
+    minimumCacheTTL: 2678400, // 31 days
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Si decides usar esta opción
+  },
 };
 
 export default nextConfig;
